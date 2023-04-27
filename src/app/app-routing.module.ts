25180446b1f2b39793b551f6e6a5856f38aa7434
gigel <gigel@mycomputer>
2023-04-27 11:37:41 +0300

    Added routing to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageListComponent } from './page-list/page-list.component';
+import { PageLoginComponent } from './page-login/page-login.component';
+import { PageReservationsComponent } from './page-reservations/page-reservations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list', component: PageListComponent},
+  {path: 'login', component: PageLoginComponent},
+  {path: 'reservations', component: PageReservationsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
