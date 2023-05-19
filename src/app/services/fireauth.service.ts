26314be86e55abb74061576d7a818148f6f03aac
gigel <gigel@mycomputer>
2023-05-19 16:23:04 +0300

    sssss

--- src/app/services/fireauth.service.ts
+++ src/app/services/fireauth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
+import { Auth, signInWithEmailAndPassword, User } from '@angular/fire/auth';
+import { FirebaseAuthError } from '../tipuri';
 
 @Injectable({
     providedIn: 'root',
   })
 export class FireAuthService {
     private auth: Auth = inject(Auth);
+    private user?: User;
+
+    errorCode: string = '0';
+    errorMessage: string = "";
+
+    async signIn(email: string, password: string){
+        signInWithEmailAndPassword(this.auth, email, password)
+        .then((userCredential) => {
+            this.user = userCredential.user;
+            this.errorCode = '0';
+            this.errorMessage = "";
+            
+        })
+        .catch((error) => {
+            let authError = error as FirebaseAuthError;
+            this.errorCode = authError.code;
+            this.errorMessage = authError.message;
+        })
+    }
 }
